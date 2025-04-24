d0b8bb8c96d00df3bfb22d510e5bf0470c16d038
RRRJJJ419 <srj4019@gmail.com>
2025-04-24 17:19:20 +0800

    Create Chapter6_99.py

---
+++ Python Code/Chapter6/Chapter6_99.py
@@ -0,0 +1,23 @@
+class People:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sleep(self):
+        print(f"{self.name} is sleeping...")
+
+    def eat(self):
+        print(f"{self.name} is eating food")
+
+
+class Student(People):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def eat(self, food):
+        print(f"{self.name} is now eating {food}")
+
+
+student_1 = Student("Ray", 25, 100)
+student_1.eat("beef")
