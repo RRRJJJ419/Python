beba8448ef745c79659139d4952f8c8aaa932985
RRRJJJ419 <srj4019@gmail.com>
2025-04-24 17:19:44 +0800

    Create Web_Scraping.py

---
+++ Python Code/爬蟲/Web_Scraping.py
@@ -0,0 +1,17 @@
+import bs4
+import requests
+
+result = requests.get(
+    "https://zh.wikipedia.org/zh-tw/%E5%93%88%E5%88%A9%C2%B7%E6%B3%A2%E7%89%B9")
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+
+image = soup.select("img.mw-file-element")
+# for i in range(len(image)):
+#    print(i)
+#    print(image[i]["src"])
+
+result = requests.get(
+    "https:" + image[14]['src'])
+with open("harry_image.png", "wb") as f:
+    f.write(result.content)
+    f.close()
