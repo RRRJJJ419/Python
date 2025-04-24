e5abfbea89dd917b5e9aed8d36afe76a9abcc32f
RRRJJJ419 <srj4019@gmail.com>
2025-04-24 17:19:22 +0800

    Create Exercise 1.py

---
+++ Python Code/Coding Practice/Exercise 1.py
@@ -0,0 +1,20 @@
+# Write a function called "printMany" that prints out integers 1, 2, 3, ..., 100.
+def printMany():
+    for i in range(1, 101):
+        print(i)
+
+
+printMany()
+
+
+# Write a function called "printEvery3" that prints out integers 1, 4, 7, 10, ..., 88.
+def printEvery3():
+    for i in range(1, 89, 3):
+        print(i)
+
+
+printEvery3()
+
+
+# Write a function called "position" that returns a tuple of the first uppercase letter and its index location. If not found, returns -1.
+def position():
